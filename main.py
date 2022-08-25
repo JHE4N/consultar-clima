0be8bc5448bfd751a1a45b10c29eb03de9fe1220
Jhean Bryan <jheanbeyan123@gmail.com>
2022-08-25 15:58:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,11 @@
 #criar uma conta no site, assim é possível obter uma key
 import requests
 def rodar():
-    #ler p apikey.txt
+    #ler o apikey.txt
     with open('apikey.txt', 'r') as arquivo:
         api_key = arquivo.read()
+
+        #obtendo as variaveis
         cidade = input('\nDeseja saber o clima de qual cidade? ')
         link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={api_key}&lang=pt_br"
         requisicao = requests.get(link)
@@ -14,8 +16,7 @@ def rodar():
         descricao = requisicao_dic['weather'][0]['description']
         temp = requisicao_dic['main']['temp'] - 273.15
 
-        #Função pra arredondar em duas casas
-        #  a temperatura
+        #Função pra arredondar em duas casas a temperatura
         def arredondar(temp):
             return float( '%g' % ( temp ) )
 
